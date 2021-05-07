bb8ece2faf003b5cf5710f80a423ae523b7ac587
Nicolas Salazar <Nico1608@Nicolass-Air.cable.rcn.com>
2021-05-07 15:13:23 -0400

    #5

--- translator/src/components/Verb.jsx
+++ translator/src/components/Verb.jsx
@@ -2,12 +2,12 @@ function Verb(props) {
   // same as const country = props.destination.fields.country;
   const { english, spanish, italian, portuguese, image } = props.verb.fields;
   return (
-    <div>
+    <div className="oneVerb">
       <h2>English: {english}</h2>
       <h3>Spanish: {spanish}</h3>
       <h3>Italian: {italian}</h3>
       <h3>Portuguese: {portuguese}</h3>
-      <img src={image}/>
+      <img src={image} />
     </div>
   )
 }
