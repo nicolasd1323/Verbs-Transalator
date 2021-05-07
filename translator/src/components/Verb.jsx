46b51820dcb26c8574dacfccb92df120ac0020ea
Nicolas Salazar <Nico1608@Nicolass-Air.cable.rcn.com>
2021-05-07 16:07:14 -0400

    #6

--- translator/src/components/Verb.jsx
+++ translator/src/components/Verb.jsx
@@ -1,14 +1,26 @@
+import axios from "axios";
+import { baseURL, config } from "../services";
+// import { Link } from "react-router-dom";
 function Verb(props) {
-  // same as const country = props.destination.fields.country;
+
+  const deleteVerb = async () => {
+    const specificURL = `${baseURL}/${props.verb.id}`
+    await axios.delete(specificURL, config);
+    props.setToggleFetch((curr) => !curr);
+  }
   const { english, spanish, italian, portuguese, image } = props.verb.fields;
   return (
     <div className="oneVerb">
+      
       <h2>English: {english}</h2>
       <h3>Spanish: {spanish}</h3>
       <h3>Italian: {italian}</h3>
       <h3>Portuguese: {portuguese}</h3>
       <img src={image} />
+      <button onClick={deleteVerb}>Incorrect</button>
     </div>
+    
   )
+    
 }
 export default Verb;
