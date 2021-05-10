74c824007fdfe4dafb4c54ef0f7ba14a394c1d3a
Nicolas Salazar <Nico1608@Nicolass-Air.cable.rcn.com>
2021-05-10 10:56:59 -0400

    changes

---
+++ translator/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import { useState} from "react";
+
+function Button(props) {
+
+  return (
+    <button>we'll put something here later</button>
+  )
+}
+export default Button;
--- translator/src/components/Dark.jsx
+++
@@ -1,9 +0,0 @@
-// import { Component } from "react";
-// class Dark extends Component {
-//   render() {
-//     return (
-//       <button onClick={this.props.changeMode}>{this.props.message}</button>
-//     );
-//   }
-// }
-// export default Dark;
